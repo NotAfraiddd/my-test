15c6d984e4474c82af319c61d0c59e1bc8b58409
ChiBao <99703812+NotAfraiddd@users.noreply.github.com>
2022-12-14 10:29:18 +0700

    todo app useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-fallthrough */
+/* eslint-disable default-case */
+import React, { useReducer, useRef } from "react";
+//use Reducer
+//1. Initialized state
+const initState = {
+  job: "",
+  jobs: [],
+};
+//2.Actions
+const SET_JOB = "set_job";
+const ADD_JOB = "add_job";
+const DELETE_JOB = "delete_job";
 
+const setJob = payload => {
+  return {
+    type: SET_JOB,
+    payload
+  }
+}
+
+const addJob = payload => {
+  return {
+    type: ADD_JOB,
+    payload
+  }
+}
+
+const deleteJob = payload => {
+  return {
+    type: DELETE_JOB,
+    payload
+  }
+}
+
+//3.Reducer
+const reducer = (state, action) => {
+  console.log('Action:', action);
+  console.log('Previous state:', state);
+
+  let newState
+  switch (action.type) {
+    case SET_JOB:
+      newState = {
+        ...state,
+        //...state là bảo lưu state cũ
+        job: action.payload
+      }
+      break;
+    case ADD_JOB:
+      newState = {
+        ...state,
+        jobs: [...state.jobs, action.payload]
+      }
+      break;
+    case DELETE_JOB:
+      const newJobs = [...state.jobs] // lấy mảng cũ
+      newJobs.splice(action.payload, 1)
+      newState = {
+        ...state,
+        jobs: newJobs
+      }
+      break;
+    default:
+      throw new Error('Invalid action.')
+  }
+
+  console.log('new state', newState);
+
+  return newState;
+}
+
+//4.Dispatcher
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState);
+  const { job, jobs } = state;
+  const inputRef = useRef();
+
+
+  const handleSubmit = () => {
+    dispatch(addJob(job))
+    dispatch(setJob(''))
+    inputRef.current.focus()
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: "0 20px" }}>
+      <h3>Todo App</h3>
+      <input
+        ref={inputRef}
+        value={job}
+        placeholder="Enter todo...."
+        onChange={e => {
+          dispatch(setJob(e.target.value));
+        }} />
+
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {
+          jobs.map((item, index) => (
+            <li key={index}> {item} <span onClick={() => dispatch(deleteJob(index))}>&times;</span></li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
